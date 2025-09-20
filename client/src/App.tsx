4510815249a4bd1f0aed1dcda5db6d6b044b85be
daiwikmh <daiwikdomain@gmail.com>
2025-09-20 17:43:50 +0530

    deployment live

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { AppLayout } from "@/pages/Layout";
-import DashboardOverview from "./pages/DashBoard";
+// import DashboardOverview from "./pages/DashBoard";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http, WagmiProvider, createConfig } from "wagmi";
